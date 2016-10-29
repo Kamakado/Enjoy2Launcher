da52d3059cdfa6b0efee199c9a35d850c6949be0
Kamakado <kamakadoproject@gmail.com>
2016-10-29 20:54:02 +0500

    first working version

--- Enjoy2 Patcher/Form1.Designer.cs
+++ Enjoy2 Patcher/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -61,22 +67,81 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(623, 381);
+            this.listBox1.Size = new System.Drawing.Size(623, 316);
             this.listBox1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 403);
+            this.label1.Location = new System.Drawing.Point(12, 331);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 3;
+            this.label1.Text = "Файл";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(93, 399);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(458, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 357);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "из";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Скорость";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(647, 463);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.progressBar1);
@@ -95,6 +160,12 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Enjoy2 Patcher/Form1.cs
+++ Enjoy2 Patcher/Form1.cs
@@ -9,11 +9,14 @@ using System.Windows.Forms;
 using System.IO;
 using Enjoy2_Launcher;
 using System.Net;
+using System.Diagnostics;
 
 namespace Enjoy2_Patcher
 {
     public partial class Form1 : Form
     {
+        WebClient webClient;    // Our WebClient that will be doing the downloading for us
+        Stopwatch sw = new Stopwatch();    // The stopwatch which we will be using to calculate the download speed
         public Form1()
         {
             InitializeComponent();
@@ -21,33 +24,13 @@ namespace Enjoy2_Patcher
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Получение патчлиста
-            WebClient test = new WebClient();
-            string temp = Encoding.ASCII.GetString(test.DownloadData(@"LINK"));
-            listBox1.Items.Add(temp);
-            //Объявление структуры патчлиста
-            CRCTools.File_Info_t[] file_cloud = null;
-            label1.Text = temp;
-            //progressBar1.Maximum = file_cloud.Length;
-            //Парсинг в структуру
-            int counter = 0;
-            while (temp.IndexOf("\"name\"=\"") != -1)
-            {
-                //Указание размерности
-                Array.Resize(ref file_cloud, (counter + 1));  
-                file_cloud[counter].filename = temp.Substring(temp.IndexOf("\"name\"=\"") + 8, (temp.IndexOf("\" \"CRC32\"=\"") - (temp.IndexOf("\"name\"=\"") + 8)));
-                file_cloud[counter].crc32 = uint.Parse(temp.Substring(temp.IndexOf("\"CRC32\"=\"") + 9, (temp.IndexOf("\" \"size\"=\"") - (temp.IndexOf("\"CRC32\"=\"") + 9))), System.Globalization.NumberStyles.HexNumber);
-                file_cloud[counter].size = Convert.ToInt64(temp.Substring(temp.IndexOf("\"size\"=\"") + 8, (temp.IndexOf("\"\r\n") - (temp.IndexOf("\"size\"=\"") + 8))));
-                listBox1.Items.Add("\"name\"=\"" + file_cloud[counter].filename + "\" \"CRC32\"=\"" + Convert.ToString(file_cloud[counter].crc32, 16) + "\" \"size\"=\"" + file_cloud[counter].size + "\"");              
-                temp = temp.Remove(0, (temp.IndexOf("\r\n") + 2));
-                label1.Text = file_cloud[counter++].filename;                
-            }     
-            //backgroundWorker1.RunWorkerAsync();
+            backgroundWorker1.RunWorkerAsync();
         }
         //"name\"=\"pack\\Zone.epk\" \"CRC32\"=\"ba0a7990\" \"size\"=\"93092608\"\r\n"
         //artpclnt.dll CRC32: 1917343657 MD5: 26B7EF7142E838B886FED8159C891A10 Size: 110592
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
+            button1.Enabled = false;
             //Получение списка файлов
             string[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*", SearchOption.AllDirectories);
             //Создание массива структур описания файлов
@@ -63,9 +46,9 @@ namespace Enjoy2_Patcher
                 //Получение информации о файле
                 FileInfo file_inform = new System.IO.FileInfo(files[i]);
                 //Запись полного пути в структуру
-                file_source[i].filepath =files[i];
+                file_source[i].filepath = files[i];
                 //Запись имени файла в структуру
-                file_source[i].filename =files[i].Replace(AppDomain.CurrentDomain.BaseDirectory,"");
+                file_source[i].filename = files[i].Replace(AppDomain.CurrentDomain.BaseDirectory, "");
                 //Запись контрольной суммы файла в структуру
                 file_source[i].crc32 = CRCTools.CalculateCRC(fsrd);
                 //Запись размера файла
@@ -77,29 +60,140 @@ namespace Enjoy2_Patcher
                 fsrd.Close();
             }
             //Получение патчлиста
-            WebClient test = new WebClient();
+            WebClient test = new WebClient();           
             string temp = Encoding.ASCII.GetString(test.DownloadData(@"http://kamakado.ru/patcher/patchlist"));
             listBox1.Items.Add(temp);
             //Объявление структуры патчлиста
             CRCTools.File_Info_t[] file_cloud = null;
-            //Указание размерности
-            Array.Resize(ref file_cloud, temp.Length);
             label1.Text = temp;
             //progressBar1.Maximum = file_cloud.Length;
             //Парсинг в структуру
             int counter = 0;
             while (temp.IndexOf("\"name\"=\"") != -1)
             {
+                //Указание размерности
+                Array.Resize(ref file_cloud, (counter + 1));
                 file_cloud[counter].filename = temp.Substring(temp.IndexOf("\"name\"=\"") + 8, (temp.IndexOf("\" \"CRC32\"=\"") - (temp.IndexOf("\"name\"=\"") + 8)));
                 file_cloud[counter].crc32 = uint.Parse(temp.Substring(temp.IndexOf("\"CRC32\"=\"") + 9, (temp.IndexOf("\" \"size\"=\"") - (temp.IndexOf("\"CRC32\"=\"") + 9))), System.Globalization.NumberStyles.HexNumber);
                 file_cloud[counter].size = Convert.ToInt64(temp.Substring(temp.IndexOf("\"size\"=\"") + 8, (temp.IndexOf("\"\r\n") - (temp.IndexOf("\"size\"=\"") + 8))));
                 listBox1.Items.Add("\"name\"=\"" + file_cloud[counter].filename + "\" \"CRC32\"=\"" + Convert.ToString(file_cloud[counter].crc32, 16) + "\" \"size\"=\"" + file_cloud[counter].size + "\"");
-                counter++;
                 temp = temp.Remove(0, (temp.IndexOf("\r\n") + 2));
-                label1.Text = file_cloud[counter].filename;
+                label1.Text = file_cloud[counter++].filename;
             }
             //Сравнение и загрузка\удаление файлов
+            for (int i = 0; i < file_source.Length; i++)
+            {
+                for (int j = 0; j < file_cloud.Length; j++)
+                {
+                    //Если имена файлов равны и не было совпадений, то
+                    if ((file_source[i].filename == file_cloud[j].filename) && (!file_cloud[j].check))
+                    {
+                        file_source[i].check = true;
+                        //Проверка на чек сумму и размер
+                        if ((file_source[i].crc32 == file_cloud[j].crc32) && (file_source[i].size == file_cloud[j].size))
+                        {
+                            file_cloud[j].check = true;
+                            break;
+                        }
+                        else
+                        {
+                            //webClient.DownloadFileAsync(new Uri(link), "sPCK.exe"); //куда сохранить, в данный момент в папку где запущена программа
+                            //Удаляем файл
+                            File.Delete(file_source[i].filepath);
+                            label1.Text = file_cloud[i].filename;
+                            //Скачиваем новый
+                            downloadFile((@"http://www.kamakado.ru/patcher/" + file_cloud[i].filename.Replace(@"\", @"/")), AppDomain.CurrentDomain.BaseDirectory + file_cloud[i].filename);
+                        }
+                    }
+                }
+                if (!file_source[i].check)
+                {
+                    try
+                    {
+                        //Удаляем файл
+                        File.Delete(file_source[i].filepath);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            progressBar1.Value = 0;
+            progressBar1.Maximum = file_cloud.Length;
+            //Докачка недостающих файлов
+            for (int i = 0; i < file_cloud.Length; i++)
+            {
+                if ((!file_cloud[i].check) && (file_cloud[i].filename != "Enjoy2 Launcher.exe"))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(file_cloud[i].filename));
+                    }
+                    catch
+                    {
+
+                    }
+                    label1.Text = file_cloud[i].filename;
+                    downloadFile((@"http://www.kamakado.ru/patcher/" + file_cloud[i].filename.Replace(@"\", @"/")), AppDomain.CurrentDomain.BaseDirectory + file_cloud[i].filename);
+                }
+                progressBar1.Value = i;
+            }
+            button1.Enabled = true;
             //for()
         }
+
+        public void downloadFile(string urlAddress, string location)
+        {
+            using (webClient = new WebClient())
+            //{
+                //webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(webClient_DownloadProgressChanged);
+                try
+                {
+                    // The variable that will be holding the url address
+                    Uri URL;
+                    // Make sure the url starts with "http://"
+                    if (!urlAddress.StartsWith("http://", StringComparison.OrdinalIgnoreCase))
+                        URL = new Uri("http://" + urlAddress);
+                    else
+                        URL = new Uri(urlAddress);
+                    // Start the stopwatch which we will be using to calculate the download speed
+                    sw.Start();
+                    // Start downloading the file
+                    webClient.DownloadFileAsync(URL, location);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            //}
+        }
+
+        // The event that will fire whenever the progress of the WebClient is changed
+        private void webClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            try
+            {
+                // Calculate download speed and output it to label3
+                if (label3.Text != (Convert.ToDouble(e.BytesReceived) / 1024 / sw.Elapsed.TotalSeconds).ToString("0"))
+                    label2.Text = (Convert.ToDouble(e.BytesReceived) / 1024 / sw.Elapsed.TotalSeconds).ToString("0.00") + " kb/s";
+
+                // Update the progressbar percentage only when the value is not the same (to avoid updating the control constantly)
+                if (progressBar2.Value != e.ProgressPercentage)
+                    progressBar2.Value = e.ProgressPercentage;
+
+                // Show the percentage on our label (update only if the value isn't the same to avoid updating the control constantly)
+                if (label3.Text != e.ProgressPercentage.ToString() + "%")
+                    label3.Text = e.ProgressPercentage.ToString() + "%";
+
+                // Update the label with how much data have been downloaded so far and the total size of the file we are currently downloading
+                label4.Text = (Convert.ToDouble(e.BytesReceived) / 1024 / 1024).ToString("0.00") + " Mb's" + "  /  " + (Convert.ToDouble(e.TotalBytesToReceive) / 1024 / 1024).ToString("0.00") + " Mb's";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
-}
+} 
